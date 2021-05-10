176ca9b4ce2f7d8b871b615b7162adf9ccbc84f1
Yurun <admin@yurunsoft.com>
2021-05-10 14:22:56 +0800

    集成组件到主仓库 (#85)

--- src/Bean/JWT.php
+++ src/Bean/JWT.php
@@ -1,5 +1,7 @@
 <?php
 
+# macro
+
 declare(strict_types=1);
 
 namespace Imi\JWT\Bean;
@@ -24,7 +26,7 @@ use Lcobucci\JWT\Validation\Constraint\SignedWith;
 use Lcobucci\JWT\Validation\Constraint\ValidAt;
 
 /**
- * @Bean("JWT")
+ * @Bean(name="JWT", recursion=false)
  */
 class JWT
 {
@@ -342,6 +344,14 @@ class JWT
 
     public function getJwtPackageVersion(): int
     {
+        # if 0
         return class_exists(\Lcobucci\JWT\Token\Parser::class) ? 4 : 3;
+        # endif
+        # if class_exists(\Lcobucci\JWT\Token\Parser::class)
+        // @phpstan-ignore-next-line
+        return 4;
+        # else
+        return 3;
+        # endif
     }
 }
--- tests/config/config.php
+++ tests/config/config.php
@@ -3,48 +3,44 @@
 declare(strict_types=1);
 
 return [
-    'configs'    => [
+    'configs'           => [
     ],
     // bean扫描目录
     // 'beanScan'    => [
     //     'Imi\JWT\Test\Test',
     // ],
-    'components'    => [
+    'components'        => [
         // 引入本组件
         'jwt'    => 'Imi\JWT',
     ],
     'ignoreNamespace'   => [
     ],
-    'beans'    => [
+    'beans'             => [
         'JWT'   => [
             'list'  => [
                 'a' => [
-                    'audience'  => 'audience_a',
-                    'subject'   => 'subject_a',
-                    'expires'   => 86400,
-                    'issuer'    => 'issuer_a',
-                    'headers'   => [
+                    'audience'      => 'audience_a',
+                    'subject'       => 'subject_a',
+                    'expires'       => 86400,
+                    'issuer'        => 'issuer_a',
+                    'headers'       => [
                         'a' => '1',
                         'b' => '2',
                     ],
-                    'tokenHandler'  => function () {
-                        return 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImEiOiIxIiwiYiI6IjIifQ.eyJhdWQiOiJhdWRpZW5jZV9hIiwic3ViIjoic3ViamVjdF9hIiwiZXhwIjo0MTE3MTkwNDAwLCJpc3MiOiJpc3N1ZXJfYSIsIm5iZiI6MTY2MDk2MzI3OC4zMDc3NzEsImp0aSI6IiIsImlhdCI6MTY2MDk2MzI3OC4zMDc3NzEsImRhdGEiOnsibWVtYmVySWQiOjE5MjYwODE3fX0.mtXSMJrvX15-Gn0h8cPXfwtnVdyJdV8eGqwD5qRqGYc';
-                    },
+                    'tokenHandler'  => static fn () => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImEiOiIxIiwiYiI6IjIifQ.eyJhdWQiOiJhdWRpZW5jZV9hIiwic3ViIjoic3ViamVjdF9hIiwiZXhwIjo0MTE3MTkwNDAwLCJpc3MiOiJpc3N1ZXJfYSIsIm5iZiI6MTY2MDk2MzI3OC4zMDc3NzEsImp0aSI6IiIsImlhdCI6MTY2MDk2MzI3OC4zMDc3NzEsImRhdGEiOnsibWVtYmVySWQiOjE5MjYwODE3fX0.mtXSMJrvX15-Gn0h8cPXfwtnVdyJdV8eGqwD5qRqGYc',
                     'privateKey'    => '1010e4c5907b331741eeacae59ec6f69',
                     'publicKey'     => '1010e4c5907b331741eeacae59ec6f69',
                 ],
                 'b' => [
-                    'audience'  => 'audience_a',
-                    'subject'   => 'subject_a',
-                    'expires'   => 86400,
-                    'issuer'    => 'issuer_a',
-                    'headers'   => [
+                    'audience'      => 'audience_a',
+                    'subject'       => 'subject_a',
+                    'expires'       => 86400,
+                    'issuer'        => 'issuer_a',
+                    'headers'       => [
                         'a' => '1',
                         'b' => '2',
                     ],
-                    'tokenHandler'  => function () {
-                        return 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImEiOiIxIiwiYiI6IjIifQ.eyJhdWQiOiJhdWRpZW5jZV9hIiwic3ViIjoic3ViamVjdF9hIiwiZXhwIjotMTM2ODg2NDAwMCwiaXNzIjoiaXNzdWVyX2EiLCJuYmYiOjAsImp0aSI6IiIsImlhdCI6MTU4MTQxMjIwMywiZGF0YSI6eyJtZW1iZXJJZCI6MTkyNjA4MTd9fQ.PAZxqO48qzu_4JBK5jyRK2nJlDNHiLkBqKQ6QLb-Duo';
-                    },
+                    'tokenHandler'  => static fn () => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImEiOiIxIiwiYiI6IjIifQ.eyJhdWQiOiJhdWRpZW5jZV9hIiwic3ViIjoic3ViamVjdF9hIiwiZXhwIjotMTM2ODg2NDAwMCwiaXNzIjoiaXNzdWVyX2EiLCJuYmYiOjAsImp0aSI6IiIsImlhdCI6MTU4MTQxMjIwMywiZGF0YSI6eyJtZW1iZXJJZCI6MTkyNjA4MTd9fQ.PAZxqO48qzu_4JBK5jyRK2nJlDNHiLkBqKQ6QLb-Duo',
                     'privateKey'    => '1010e4c5907b331741eeacae59ec6f69',
                     'publicKey'     => '1010e4c5907b331741eeacae59ec6f69',
                 ],
